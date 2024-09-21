64ba00b33d7c8442d5a19a482b564fc45e82c8f7
DELL <DELL@DESKTOP-RQ7104O>
2024-09-21 14:21:52 +0530

    This is my task related to Hybrid Luma Testng project

---
+++ src/test/java/Luma_Testcases/Sum.java
@@ -0,0 +1,14 @@
+package Luma_Testcases;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		a=10;
+		b=20;
+		System.out.println(a+b);
+
+	}
+
+}
